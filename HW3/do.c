eabc11b41be4834073148e0e342fd191a4c64d32
barnecle <barnecle@iu.edu>
2019-09-25 15:55:39 +0000

    add solution to HW3

---
+++ HW3/do.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  int i = 0;
+
+  do{
+    
+    printf("i is %d\n", i);
+    i++;
+  }while(i<10);
+  
+  
+}
---
+++ HW3/for.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  int i = 0;
+
+  for(i; i<10; i++)
+    {
+      printf("i is %d\n", i);
+    }
+  
+}
