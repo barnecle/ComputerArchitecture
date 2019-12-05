968fdda49db219a65d36e800ca7a92574aa359fa
Clare Barnes <barnecle@copperhead.sice.indiana.edu>
2019-12-05 15:08:31 -0500

    commented code

--- HW6/prodcons_q.c
+++ HW6/prodcons_q.c
@@ -2,34 +2,37 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <time.h>
 
+//for use within function
 sem_t upper;
 sem_t lower;
 sem_t cons;
 
+//for use between functions
 sem_t mutexi;
 sem_t mutexj;
 sem_t mutexk;
 
 char buf[5];
-int h = 0;
+int h = 0; //for reader to call either randl or randU
 
 void *randl(void* arg){
   int i;
-  sem_wait(&lower);
-  sem_wait(&mutexi);
+  sem_wait(&mutexi); //wait for reader to empty buf
+  sem_wait(&lower); //only one thread at a time
   for(i = 0; i < 5; i++){
-	buf[i] = rand()%26 + 'a';
+    buf[i] = rand()%26 + 'a'; //random letter
         printf("Thread %d writing %c\n", pthread_self(), buf[i]);
   } 
   sem_post(&lower);
-  sem_post(&mutexk);
+  sem_post(&mutexk); //signal to reader
 
 }
 void *randU(void* arg){
   int j;
-  sem_wait(&mutexj);
-  sem_wait(&upper);
+  sem_wait(&mutexj); //wait for reader after first write
+  sem_wait(&upper); //only one thread can write at a time
   for(j = 0; j < 5; j++){
 	buf[j] = rand()%26 + 'A';
         printf("Thread %d writing %c\n", pthread_self(), buf[j]);
@@ -40,15 +43,15 @@ void *randU(void* arg){
 }
 void *reader(void* arg){
   int k;
-  sem_wait(&mutexk);
-  sem_wait(&cons);
+  sem_wait(&mutexk); //wait for full buffer
+  sem_wait(&cons); //one reader at a time
   for(k = 0; k < 5; k++){
         printf("\t\tThread %d reading %c\n", pthread_self(), buf[k]);
   }
-  h++;
-  sem_post(&cons);
-  if(h< 2){
-	sem_post(&mutexj);
+  h++; //keep track of amount of times reader operates
+  sem_post(&cons); 
+  if(h< 2){ //after the 2 randU() threads execute go to randl()
+    sem_post(&mutexj); 
   }else{
 	sem_post(&mutexi);
   }
@@ -57,19 +60,20 @@ int main(){
   sem_init(&upper, 1, 1);
   sem_init(&lower, 1, 1);
   sem_init(&cons, 1, 1);
-  sem_init(&mutexi, 1, 0);
-  sem_init(&mutexj, 1, 1);
-  sem_init(&mutexk, 1, 0);
+  sem_init(&mutexi, 1, 0); //second writer
+  sem_init(&mutexj, 1, 1); //first writer
+  sem_init(&mutexk, 1, 0); //reader
 
   pthread_t t[8];
   int i;
+  srand(time(0)); //seed for rand()
 
   for(i = 0; i<8; i++){
-	if(i<2){
+    if(i<2){ //two threads to randU
 	  pthread_create(&t[i], NULL, &randU, NULL);
-  	}else if(i<4){
+    }else if(i<4){ //two threads to randl
 	   pthread_create(&t[i], NULL, &randl, NULL);
-	}else{
+    }else{ //4 threads to reader
 	   pthread_create(&t[i], NULL, &reader, NULL);
   	}
   }
