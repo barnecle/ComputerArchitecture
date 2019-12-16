737739208b9d18acc1c7b30a936cd4a6f8f4f2aa
Clare Barnes <barnecle@silo.soic.indiana.edu>
2019-12-16 17:19:22 -0500

    update

--- Lab5/rd_wr.c
+++ Lab5/rd_wr.c
@@ -5,70 +5,54 @@
 
 float stockval;
 float a = 0.45;
-int j = 0;
-int c;
-sem_t s1;
-sem_t s2;
-sem_t reader;
-sem_t writer;
+int count = 0;
+sem_t buf;
+sem_t mutex;
 
 void *read(){
   int i;
-  int k;
-  int val;
-  while(j != 16){
-	 
-	  for(c = 0; c<8; c++){
-	    sem_wait(&s1); //wait for writers to write
 
-	    printf("\n"); //these printf("\n"); lines for some reason, are needed to make the program work
-	    sem_wait(&reader); //only one reader prints at a time
-	    for(i = 0; i<5; i++){
- 	    	    printf("\t\tThread %d Stock Value = %f\n", pthread_self(), stockval);
-	    }
-	    sem_post(&reader);
-	    printf("\n");
-	  }
-
-	  sem_post(&s2); //writers allowed to write
-
-
+  for(i = 0; i<5; i++){
+	sem_wait(&mutex); //only one reader has access to count at a time
+	count++;
+	if(count == 1){ //if the first reader, wait for buffer to be filled
+		sem_wait(&buf);
+	}
+	sem_post(&mutex); //release count section
+	printf("\t\tThread %d Stock Value = %f\n", pthread_self(), stockval);
+	sem_wait(&mutex); //lock to modify count
+	count--;
+	if(count == 0){ //if last reader, release buffer
+		sem_post(&buf);
+	}
+	sem_post(&mutex); //release count
   }
+
 	
 }
 void *write(){
   int i;
-  int k;
-
+ 
   for(i = 0; i<4; i++){
-	sem_wait(&s2); //after first writing, wait for readers
-	sem_wait(&writer); //only one writer writes at time
-	printf("\n");
+	sem_wait(&buf); //wait for buffer
 	stockval = (float)rand() * a; //assign random float
 	printf("Thread %d writing value %f\n", pthread_self(), stockval);
-	j++; //increment count
-	sem_post(&writer);
-	printf("\n");
-	for(k = 0; k <8; k++){ //post 8 times for 8 readers
-        	sem_post(&s1);
-        }
+
+	sem_post(&buf); //release buffer for next thread
+        
 
   }
   
-  if(j == 15){ //for the last writer, post one more time to let readers leave
-	for(i = 0; i <8; i++){
-	  sem_post(&s1);
-	}
-  }
+
 }
 
 int main(){
   pthread_t writers[12];
   int i;
-  sem_init(&s1, 1, 0);
-  sem_init(&s2, 1, 1);
-  sem_init(&reader, 1, 1);
-  sem_init(&writer, 1, 1);
+  sem_init(&buf, 1, 1);
+  sem_init(&mutex, 1, 1);
+ 
+
   for(i = 0; i<12; i++){
      if(i<4){ //four writers
 	pthread_create(&writers[i], NULL, &write, NULL);
@@ -81,9 +65,7 @@ int main(){
         pthread_join(writers[i], NULL);
   }
 
-  sem_destroy(&s1);
-  sem_destroy(&s2);
-  sem_destroy(&reader);
-  sem_destroy(&writer);
+  sem_destroy(&mutex);
+  sem_destroy(&buf); 
   exit(0);
 }
